30082e00b8927ed2fc229d3455115e8aac64b9bf
Piotr Kania <piotr@trener-it.pl>
2026-03-23 12:24:16 +0100

    add mod name

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,10 @@ import { Component, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+interface ContactInterface {
+  firstName: string
+}
+
 @Component({
   imports: [CommonModule, FormsModule],
   selector: 'app-root',
@@ -14,7 +18,25 @@ export class App {
   
   data: string[] = ["Piotr", "Anna", "Adam"];
 
+  contactName?: ContactInterface = {firstName: ''};
+
+  modIndex: number = -1;
+
+  ngOnInit() {
+    this.contactName = {
+      firstName: ''
+    }
+  }
+
   setNewName(data: NgForm) {
+
+    if(this.modIndex >= 0) {
+      this.data[this.modIndex] = data.value.firstname;
+      this.modIndex = -1;
+      data.reset();
+      return;
+    }
+
     console.log(data.value.firstname);
     const imie: string = data.value.firstname;
 
@@ -28,7 +50,11 @@ export class App {
   }
 
   modElem(i: number): void {
-    
+    const name = this.data[i];
+    this.modIndex = i;
+    this.contactName = {
+      firstName: name
+    }
   }
 
 
